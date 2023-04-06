28d322d22d6d1f38aea6b3c27e7477b7a3ca9da1
Gianluigi <50233545+RallyTuning@users.noreply.github.com>
2023-04-06 19:16:18 +0200

    You know what? Object is better
    You can convert it later, if you need, but at least game details deserialization will not go in error. (also if you don't use it. Take this as "just in case you will use it one day")

--- FreeSteamGames_TelegramBot/SteamCrawler/Models/Steam/Data.cs
+++ FreeSteamGames_TelegramBot/SteamCrawler/Models/Steam/Data.cs
@@ -15,7 +15,7 @@ public class Data
     public int SteamAppid { get; set; }
 
     [JsonPropertyName("required_age")]
-    public string RequiredAge { get; set; }
+    public object RequiredAge { get; set; }
 
     [JsonPropertyName("is_free")]
     public bool IsFree { get; set; }
